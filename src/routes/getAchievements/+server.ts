47dc1adbfd550c0551079011cdacae8a3c6b94b0
William Ekdahl <viljam89@hotmail.com>
2023-05-03 22:59:56 +0200

    Imported jsdom

--- src/routes/getAchievements/+server.ts
+++ src/routes/getAchievements/+server.ts
@@ -1,4 +1,4 @@
-import { STEAM_KEY } from '$env/static/private';
+import { JSDOM } from 'jsdom'
 
 export async function POST({ request }) {
 	const input = await request.json();
@@ -9,7 +9,7 @@ export async function POST({ request }) {
 	const achiev = await fetchAchievements(input.userId, input.appId);
 	console.log('ACHIEVEMENTS');
 	console.log(achiev);
-	
+
 	return new Response(JSON.stringify({ body: achiev }));
 }
 
@@ -18,8 +18,29 @@ async function fetchAchievements(id: String, game: String) {
 	const url = `https://steamcommunity.com/stats/${game}/achievements`;
 	const response = await fetch(url);
 	const res = await response.text();
-	console.log("RESULT FROM ACHIEVEMENTS FETCH")
-	console.log(res)
+	//console.log("RESULT FROM ACHIEVEMENTS FETCH")
+	//console.log(res)
+
+	let test = parseString(res);
 
 	return res;
+}
+
+function parseString(input: string) {
+	const dom = new JSDOM(input);
+	const parser = new dom.window.DOMParser();
+	const doc = parser.parseFromString(input, 'text/html');
+
+	const achievementDivs = doc.querySelectorAll('#mainContents .achieveRow');
+
+	const achievements: { title: string, description: string }[] = [];
+	achievementDivs.forEach((div) => {
+		const title = div.querySelector('.achieveTxt h3')?.textContent?.trim() || '';
+		const description = div.querySelector('.achieveTxt h5')?.textContent?.trim() || '';
+		achievements.push({ title, description });
+	});
+
+	console.log("ACHIEVEMENT NAMES")
+	console.log(achievements)
+	return achievements;
 }
