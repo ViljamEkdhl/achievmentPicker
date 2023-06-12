05b2f856ee6e0b51c96c8fab6480a308147e5627
William Ekdahl <viljam89@hotmail.com>
2023-06-12 13:02:43 +0200

    small change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import GetSteamId from "../components/getSteamId.svelte";
-	import { dev } from "$app/environment";
-	import { inject } from '@vercel/analytics'
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 
 	inject({ mode: dev ? 'development' : 'production' });
 
