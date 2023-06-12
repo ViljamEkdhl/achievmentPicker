665a8f534af09da2afaa49382e53bd5808f2f006
William Ekdahl <viljam89@hotmail.com>
2023-06-12 12:56:48 +0200

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import GetSteamId from "../components/getSteamId.svelte";
+	import { dev } from "$app/environment";
+	import { inject } from '@vercel/analytics'
 
+	inject({ mode: dev ? 'development' : 'production' });
 
 </script>
 
